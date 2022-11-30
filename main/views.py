9c7bea9d84c7875466f174029ca614013f22e4bb
ciaransanders <ciaransanders@gmail.com>
2022-11-29 21:28:27 -0700

    completely barebones html for some testing

--- main/views.py
+++ main/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from main.models import Board, Submission
+
+
 def landing(request):
     return render(
         request,
         'landing.html',
+        {
+            'boards': Board.objects.all(),
+            'recent_submissions': Submission.objects.order_by('date')[:5],
+        }
     )
