4f8679452cdd3dc453c531c404e0656b8d76659b
ciaransanders <ciaransanders@gmail.com>
2022-11-29 20:33:31 -0700

    added Board and Submission models to main

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Board(models.Model):
+    BOSS, MINIGAME = range(2)
+    BOARD_CHOICES = (
+        (BOSS, 'Boss'),
+        (MINIGAME, 'Minigame'),
+    )
+    TIME, OTHER = range(2)
+    METRIC_CHOICES = (
+        (TIME, 'Time'),
+        (OTHER, 'Other'),
+    )
+    name = models.CharField(max_length=256)
+    type = models.IntegerField(choices=BOARD_CHOICES, default=BOSS)
+    metric = models.IntegerField(choices=METRIC_CHOICES, default=TIME)
+
+
+class Submission(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    board = models.ForeignKey('main.Board', on_delete=models.CASCADE)
+    value = models.DecimalField(max_digits=6, decimal_places=2)
