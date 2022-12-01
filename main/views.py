f512d1f7da08894385f32e40cf27b4cbe7a395a9
ciaransanders <ciaransanders@gmail.com>
2022-12-01 11:43:10 -0700

    added static files for logos in main/static/ ; base layout for landing dashboard ; testing out a navbar style ; added various leaderbaords to front page

--- main/views.py
+++ main/views.py
@@ -1,14 +1,25 @@
+from collections import Counter
+
+from django.contrib.auth.models import User
+from django.db.models import Max
 from django.shortcuts import render
 
-from main.models import Board, Submission
+from main.models import BoardCategory, Submission
 
 
 def landing(request):
+    temp = Submission.objects.values('board').annotate(Max('value')).values_list('user', flat=True)
+    first_places = [
+        {'user': User.objects.get(pk=pk), 'val': val}
+        for pk, val in Counter(temp).most_common(5)
+    ]
+
     return render(
         request,
         'landing.html',
         {
-            'boards': Board.objects.all(),
+            'board_categories': BoardCategory.objects.all(),
             'recent_submissions': Submission.objects.order_by('date')[:5],
+            'first_places': first_places,
         }
     )
