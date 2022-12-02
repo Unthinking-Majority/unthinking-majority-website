cd0b19bb199b09f5b95b1447acf74dc34699aaaf
ciaransanders <ciaransanders@gmail.com>
2022-12-02 12:11:53 -0700

    added value_display template tag for converting record values to appropriate viewing form

--- main/templatetags/main_extras.py
+++ main/templatetags/main_extras.py
@@ -1,6 +1,6 @@
 from django import template
 
-from main.models import BoardCategory
+from main.models import BoardCategory, Board
 
 register = template.Library()
 
@@ -8,3 +8,13 @@ register = template.Library()
 @register.inclusion_tag('navbar.html')
 def navbar():
     return {'board_categories': BoardCategory.objects.all()}
+
+
+@register.filter
+def value_display(value, metric):
+    if metric == Board.TIME:
+        minutes = int(value // 60)
+        seconds = value % 60
+        return f"{minutes}:{seconds}"
+    else:
+        return value
