53042c1ed274958d51ade848b72c4ef7bde2688c
ciaransanders <ciaransanders@gmail.com>
2022-12-03 18:27:47 -0700

    un-did the refactor i did for  settings

--- um/settings/__init__.py
+++
@@ -1,7 +0,0 @@
-from um.settings.base import *
-
-debug = os.environ.get('DEBUG', 'false').lower() == 'true'
-if debug:
-    from um.settings.dev import *
-else:
-    from um.settings.production import *
