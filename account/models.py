2317874d00792d9b952c93238b6e9e53c5c2b2aa
ciaransanders <ciaransanders@gmail.com>
2022-12-04 16:54:37 -0700

    added icon field to Account model

--- account/models.py
+++ account/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Account(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True)
     name = models.CharField(max_length=256, help_text='In game name.')  # TODO might want to track name changes
+    icon = models.ImageField(upload_to='account/icons/', null=True, blank=True)
 
     def __str__(self):
         return self.name
