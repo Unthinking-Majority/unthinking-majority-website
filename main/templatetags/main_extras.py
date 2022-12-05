fedafd397c3847c21e008fe6148c7395aab2252e
ciaransanders <ciaransanders@gmail.com>
2022-12-04 17:21:38 -0700

    updated pet leaderboard

--- main/templatetags/main_extras.py
+++ main/templatetags/main_extras.py
@@ -1,5 +1,7 @@
 from django import template
+from django.db.models import Count
 
+from account.models import Account
 from main.models import BoardCategory, Board
 
 register = template.Library()
@@ -8,6 +10,11 @@ register = template.Library()
 @register.inclusion_tag('navbar.html')
 def navbar():
     return {'board_categories': BoardCategory.objects.all()}
+
+
+@register.inclusion_tag('dashboard/pets_leaderboard.html')
+def pets_leaderboard():
+    return {'accounts': Account.objects.annotate(num_pets=Count('pets')).order_by('num_pets').prefetch_related('pets')}
 
 
 @register.filter
