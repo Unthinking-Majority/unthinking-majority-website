cd368d0e9276ce259667a4437c32d31564cc9dcc
ciaransanders <ciaransanders@gmail.com>
2022-12-05 09:33:54 -0700

    added account urls to um.urls

--- um/urls.py
+++ um/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('account.urls')),
     path('__reload__/', include('django_browser_reload.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
