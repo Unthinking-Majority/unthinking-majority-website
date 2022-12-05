e045119337ebe4f60625a6fa92b11071d4e4c396
ciaransanders <ciaransanders@gmail.com>
2022-12-05 14:52:17 -0700

    inital password reset form ; password reset link added to login.html ; view updated to work with new templates ; account/registration/password_reset_email.html added

--- account/urls.py
+++ account/urls.py
@@ -26,7 +26,10 @@ urlpatterns = [
     ),
     path(
         'password_change/',
-        auth_views.PasswordChangeView.as_view(template_name='account/registration/password_change_form.html', success_url=reverse_lazy('accounts:password_change_done')),
+        auth_views.PasswordChangeView.as_view(
+            template_name='account/registration/password_change_form.html',
+            success_url=reverse_lazy('accounts:password_change_done')
+        ),
         name='password_change'
     ),
     path(
@@ -36,7 +39,11 @@ urlpatterns = [
     ),
     path(
         'password_reset/',
-        auth_views.PasswordResetView.as_view(template_name='account/registration/password_reset_form.html'),
+        auth_views.PasswordResetView.as_view(
+            template_name='account/registration/password_reset_form.html',
+            success_url=reverse_lazy('accounts:password_reset_done'),
+            email_template_name='account/registration/password_reset_email.html'
+        ),
         name='password_reset'
     ),
     path(
