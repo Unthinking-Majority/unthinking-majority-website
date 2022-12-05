21ab58b90db90d78d87013cfb85e9807af005d50
ciaransanders <ciaransanders@gmail.com>
2022-12-05 13:27:35 -0700

    added board_url templatetag ; navbar now highlights current page in navbar

--- main/templatetags/main_extras.py
+++ main/templatetags/main_extras.py
@@ -10,14 +10,20 @@ register = template.Library()
 @register.inclusion_tag('navbar.html', takes_context=True)
 def navbar(context):
     return {
+        'request': context['request'],
+        'board': context.get('board', None),
         'board_categories': BoardCategory.objects.all(),
-        'request': context['request']
     }
 
 
 @register.inclusion_tag('dashboard/pets_leaderboard.html')
 def pets_leaderboard():
     return {'accounts': Account.objects.annotate(num_pets=Count('pets')).order_by('-num_pets').prefetch_related('pets')[:5]}
+
+
+@register.filter
+def board_url(board):
+    return f'/board/{board.category.slug}/{board.slug}'
 
 
 @register.filter
