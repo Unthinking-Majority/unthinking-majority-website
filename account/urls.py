7546f42fc920b639ead27670bb32cf5a863bdb19
ciaransanders <ciaransanders@gmail.com>
2022-12-05 14:33:26 -0700

    added initial password change done page ; PassWordChangeView now redirects correctly to PasswordChanceDoneView

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 
 from account import views
 
@@ -26,7 +26,7 @@ urlpatterns = [
     ),
     path(
         'password_change/',
-        auth_views.PasswordChangeView.as_view(template_name='account/registration/password_change_form.html'),
+        auth_views.PasswordChangeView.as_view(template_name='account/registration/password_change_form.html', success_url=reverse_lazy('accounts:password_change_done')),
         name='password_change'
     ),
     path(
