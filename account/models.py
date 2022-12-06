300d4e4d253747e17c7946c6555b89ec90161c23
ciaransanders <ciaransanders@gmail.com>
2022-12-06 14:45:05 -0700

    removed icon field from Account model

--- account/models.py
+++ account/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Account(models.Model):
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=256, help_text='In game name.', unique=True)  # TODO might want to track name changes
-    icon = models.ImageField(upload_to='account/icons/', null=True, blank=True)
 
     def __str__(self):
         return self.name
