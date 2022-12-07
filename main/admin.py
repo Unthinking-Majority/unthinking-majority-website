e72a60b0db6f17b3995edbdeb93f9c2ee798e113
ciaransanders <ciaransanders@gmail.com>
2022-12-06 21:16:07 -0700

    fleshed out SubmissionAdmin ; added autocomplete_filters.py in main with AccountFilter

--- main/admin.py
+++ main/admin.py
@@ -1,26 +1,46 @@
 from django.contrib import admin
 
 from main import models
-
-
-class BoardAdmin(admin.ModelAdmin):
-    search_fields = ['name']
-    prepopulated_fields = {'slug': ('name', )}
+from main.autcomplete_filters import AccountFilter
 
 
 class BoardCategoryAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['name']
+
+
+class BoardAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
 
 
 class SubmissionAdmin(admin.ModelAdmin):
     autocomplete_fields = ['account', 'board']
+    list_display = ['account_name', 'board', 'value', 'proof', 'date', 'accepted']
+    list_editable = ['accepted']
+    list_filter = [AccountFilter, 'board', 'date', 'accepted']
+    readonly_fields = ['account_name']
+    search_fields = ['account__name', 'value']
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'account',
+                ('board', 'value'),
+                'proof',
+                'accepted',
+            )
+        }),
+    )
+
+    def account_name(self, obj):
+        return obj.account.name
 
 
 class PetAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['name']
 
 
 admin.site.register(models.Board, BoardAdmin)
 admin.site.register(models.BoardCategory, BoardCategoryAdmin)
-admin.site.register(models.Submission, SubmissionAdmin)
 admin.site.register(models.Pet, PetAdmin)
+admin.site.register(models.Submission, SubmissionAdmin)
