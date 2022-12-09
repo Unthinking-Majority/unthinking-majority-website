dd0466ac7a7de1981f7eb791f310bbc4cf372896
ciaransanders <ciaransanders@gmail.com>
2022-12-09 16:51:08 -0700

    added check in base.js to prevent error

--- main/static/js/base.js
+++ main/static/js/base.js
@@ -1,6 +1,10 @@
-document.querySelector('.file-input').onchange = function () {
-    let file_name = this.files.item(0).name;
-    this.labels.forEach(label => {
-        label.querySelector('.file-label').innerText = file_name;
-    });
-};
+let file_inputs = document.querySelector('.file-input');
+
+if (file_inputs) {
+    file_inputs.onchange = function () {
+        let file_name = this.files.item(0).name;
+        this.labels.forEach(label => {
+            label.querySelector('.file-label').innerText = file_name;
+        });
+    };
+}
