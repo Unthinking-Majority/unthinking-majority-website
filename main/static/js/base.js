12acb466081fb306ab8c509adadd386f5ebd6d51
ciaransanders <ciaransanders@gmail.com>
2022-12-11 12:38:18 -0700

    added DOMContentLoaded event wrapped around code in base.js

--- main/static/js/base.js
+++ main/static/js/base.js
@@ -1,10 +1,13 @@
-let file_inputs = document.querySelector('.file-input');
+document.addEventListener("DOMContentLoaded", () => {
+    let file_inputs = document.querySelector('.file-input');
 
-if (file_inputs) {
-    file_inputs.onchange = function () {
-        let file_name = this.files.item(0).name;
-        this.labels.forEach(label => {
-            label.querySelector('.file-label').innerText = file_name;
-        });
-    };
-}
+    if (file_inputs) {
+        file_inputs.onchange = function () {
+            let file_name = this.files.item(0).name;
+            this.labels.forEach(label => {
+                label.querySelector('.file-label').innerText = file_name;
+                console.log('huh');
+            });
+        };
+    }
+});
