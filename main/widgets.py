96ac265ca9fea546d66b2ce86c0c250eb6664604
ciaransanders <ciaransanders@gmail.com>
2022-12-27 11:32:27 -0800

    added label parameter to AutocompleteSelectWidget

--- main/widgets.py
+++ main/widgets.py
@@ -4,15 +4,18 @@ from django import forms
 class AutocompleteSelectWidget(forms.Select):
     autocomplete_url = ''
     placeholder = ''
+    label = ''
     template_name = 'widgets/autocomplete_select.html'
 
-    def __init__(self, autocomplete_url='', placeholder=''):
+    def __init__(self, autocomplete_url='', placeholder='', label=''):
         self.autocomplete_url = autocomplete_url
         self.placeholder = placeholder
+        self.label = label
         super(AutocompleteSelectWidget, self).__init__()
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
         context['autocomplete_url'] = self.autocomplete_url
         context['placeholder'] = self.placeholder
+        context['label'] = self.label
         return context
