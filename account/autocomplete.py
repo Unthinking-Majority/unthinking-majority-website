d2f0c7d37e77ee47437189b115636c884b993526
ciaransanders <ciaransanders@gmail.com>
2022-12-27 11:22:39 -0800

    small account cleanup in autocompletes

--- account/autocomplete.py
+++ account/autocomplete.py
@@ -1,12 +1,12 @@
 from dal import autocomplete
 
-from account.models import Account
+from account import models
 
 
 class AccountAutocomplete(autocomplete.Select2QuerySetView):
 
     def get_queryset(self):
-        qs = Account.objects.all()
+        qs = models.Account.objects.all()
 
         if self.q:
             qs = qs.filter(name__istartswith=self.q)
--- account/urls/autocomplete_urls.py
+++ account/urls/autocomplete_urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 
 from account import autocomplete
 
-app_name = 'accounts'
-
 autocomplete_urlpatterns = [
-    path('account-autocomplete/',
-         autocomplete.AccountAutocomplete.as_view(),
-         name='account-autocomplete'),
+    path('account-autocomplete/', autocomplete.AccountAutocomplete.as_view(), name='account-autocomplete'),
 ]
