cdd798a53f6b66526fca1d2e36c36b360a940c17
ciaransanders <ciaransanders@gmail.com>
2022-12-31 11:16:12 -0800

    added textarea_input template tag and replaced textareas with it

--- theme/templatetags/um_components.py
+++ theme/templatetags/um_components.py
@@ -11,3 +11,7 @@ def page_header(text, icon=None):
 @register.inclusion_tag('theme/components/simple_input.html')
 def simple_input(field, label=None, prefix=''):
     return {'field': field, 'label': label, 'prefix': prefix}
+
+@register.inclusion_tag('theme/components/textarea_input.html')
+def textarea_input(field, label=None, prefix=''):
+    return {'field': field, 'label': label, 'prefix': prefix}
