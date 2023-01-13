b2ac7ee7c482d4b7a62fe2d144d5f49451cc41ef
ciaransanders <ciaransanders@gmail.com>
2023-01-13 15:53:15 -0700

    temp

--- account/autocomplete.py
+++ account/autocomplete.py
@@ -5,5 +5,6 @@ from account import models
 
 
 def account_autocomplete(request):
-    accounts = models.Account.objects.filter(active=True).annotate(text=F('name')).values('id', 'text')
+    # accounts = models.Account.objects.filter(active=True).annotate(text=F('name')).values('id', 'text')
+    accounts = models.Account.objects.annotate(text=F('name')).values('id', 'text')
     return JsonResponse(list(accounts), safe=False)
