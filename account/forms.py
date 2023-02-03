9e0eaebac82cd2d5752c0f1215935bd9267d642e
ciaransanders <ciaransanders@gmail.com>
2023-02-03 10:52:19 -0700

    removed email field from account creation

--- account/forms.py
+++ account/forms.py
@@ -7,16 +7,6 @@ from account import models
 from main import widgets
 
 
-class CreateUserForm(UserCreationForm):
-    email = forms.EmailField(label='Email', required=False)
-
-    def save(self, commit=True):
-        user = super(CreateUserForm, self).save(commit=commit)
-        user.email = self.cleaned_data['email']
-        user.save()
-        return user
-
-
 class CreateAccountForm(forms.Form):
     username = forms.CharField(
         label='Username',
@@ -27,11 +17,6 @@ class CreateAccountForm(forms.Form):
     )
     account = forms.ModelChoiceField(
         queryset=models.Account.objects.all()
-    )
-    email = forms.EmailField(
-        label='Email',
-        help_text='(Optional) In the event you lose your password, we can use your email to reset it.',
-        required=False
     )
     password1 = forms.CharField(
         label='Password',
@@ -65,13 +50,13 @@ class CreateAccountForm(forms.Form):
 
     def clean(self):
         cleaned_data = super(CreateAccountForm, self).clean()
-        user_form = CreateUserForm(cleaned_data)
+        user_form = UserCreationForm(cleaned_data)
         if not user_form.is_valid():
             raise ValidationError(user_form.errors)
         return cleaned_data
 
     def form_valid(self):
-        user_form = CreateUserForm(self.cleaned_data)
+        user_form = UserCreationForm(self.cleaned_data)
         user = user_form.save(commit=True)
 
         account = self.cleaned_data['account']
