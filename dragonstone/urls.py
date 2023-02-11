d83bbbfe0bdaaa9a0945377e09619025f9e301eb
ciaransanders <ciaransanders@gmail.com>
2023-02-11 15:59:07 -0700

    wrapped access to dragonstone behind is_staff

--- dragonstone/urls.py
+++ dragonstone/urls.py
@@ -1,7 +1,8 @@
+from django.contrib.admin.views.decorators import staff_member_required as smr
 from django.urls import path
 
 from dragonstone import views
 
 urlpatterns = [
-    path('submit/dragonstone/', views.DragonstoneSubmissionWizard.as_view(), name='submit-dragonstone'),
+    path('submit/dragonstone/', smr(views.DragonstoneSubmissionWizard.as_view()), name='submit-dragonstone'),
 ]
