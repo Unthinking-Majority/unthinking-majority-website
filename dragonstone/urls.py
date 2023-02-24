1782f6221a1c5646dc004164842ae2f28a745593
ciaransanders <ciaransanders@gmail.com>
2023-02-23 20:24:22 -0700

    dragonstone now available to non-staff members

--- dragonstone/urls.py
+++ dragonstone/urls.py
@@ -1,8 +1,7 @@
-from django.contrib.admin.views.decorators import staff_member_required as smr
 from django.urls import path
 
 from dragonstone import views
 
 urlpatterns = [
-    path('submit/dragonstone/', smr(views.DragonstoneSubmissionWizard.as_view()), name='submit-dragonstone'),
+    path('submit/dragonstone/', views.DragonstoneSubmissionWizard.as_view(), name='submit-dragonstone'),
 ]
