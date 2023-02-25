d6511c7129f3106ded3b8064f97f55e1d5058ec6
ciaransanders <ciaransanders@gmail.com>
2023-02-24 20:32:53 -0700

    changed admin site_title, so we are more official!

--- um/urls.py
+++ um/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'UM Administration'
+
 urlpatterns = [
     path('', include('main.urls')),
     path('', include('achievements.urls')),
