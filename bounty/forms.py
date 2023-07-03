3da7d2a7c597958f1d34535cef4403932c926143
Ciaran Sanders <ciaransanders@gmail.com>
2023-07-03 16:14:16 -0600

    fixed Bounty admin form validation to exclude itself when checking for bounties within the same date range

--- bounty/forms.py
+++ bounty/forms.py
@@ -10,18 +10,22 @@ class BountyAdminForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = super().clean()
-        start_date = cleaned_data.get('start_date')
-        end_date = cleaned_data.get('end_date')
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
 
         if start_date and end_date:
             if start_date >= end_date:
-                raise ValidationError('The Start date must be less than the end date.')
+                raise ValidationError("The Start date must be less than the end date.")
 
             if models.Bounty.objects.filter(
-                Q(start_date__range=[start_date, end_date]) |
-                Q(end_date__range=[start_date, end_date]) &
-                ~Q(id=self.instance.id)
+                (
+                    Q(start_date__range=[start_date, end_date])
+                    | Q(end_date__range=[start_date, end_date])
+                )
+                & ~Q(id=self.instance.id)
             ).exists():
-                raise ValidationError('A Bounty event is already scheduled within this time range.')
+                raise ValidationError(
+                    "A Bounty event is already scheduled within this time range."
+                )
 
         return cleaned_data
