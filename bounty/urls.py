44406a57de2e5b495beb4b6dcec4aa5842d34c28
Ciaran Sanders <ciaransanders@gmail.com>
2023-10-29 20:12:51 -0600

    bounty event code available only for superusers/staff members at the moment until ready for release

--- bounty/urls.py
+++ bounty/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import path
 
 from bounty import views
@@ -5,8 +6,24 @@ from bounty import views
 app_name = "bounty"
 
 urlpatterns = [
-    path("", views.CurrentBountyView.as_view(), name="current-bounty"),
-    path("index/", views.BountyIndexView.as_view(), name="bounty-index"),
-    path("<int:id>/", views.BountyView.as_view(), name="bounty-view"),
-    path("rules/", views.BountyRulesView.as_view(), name="bounty-rules"),
+    path(
+        "",
+        staff_member_required(views.CurrentBountyView.as_view()),
+        name="current-bounty",
+    ),
+    path(
+        "index/",
+        staff_member_required(views.BountyIndexView.as_view()),
+        name="bounty-index",
+    ),
+    path(
+        "<int:id>/",
+        staff_member_required(views.BountyView.as_view()),
+        name="bounty-view",
+    ),
+    path(
+        "rules/",
+        staff_member_required(views.BountyRulesView.as_view()),
+        name="bounty-rules",
+    ),
 ]
