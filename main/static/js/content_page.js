e4b9bfd9c38de893b3ccee4c0a467cc1679bdd4a
Ciaran Sanders <ciaransanders@gmail.com>
2023-12-15 16:04:48 -0700

    prototype sticky sidebar

--- main/static/js/content_page.js
+++ main/static/js/content_page.js
@@ -1,1 +1,11 @@
-console.log("hello");
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
