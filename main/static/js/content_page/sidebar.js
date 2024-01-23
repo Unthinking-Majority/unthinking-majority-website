404541b5ea7c0bd3ee6e4544294539442bd55031
Ciaran Sanders <ciaransanders@gmail.com>
2024-01-23 13:26:15 -0700

    sidebar styling update

--- main/static/js/content_page/sidebar.js
+++ main/static/js/content_page/sidebar.js
@@ -1,7 +1,3 @@
-function convertRemToPixels(rem) {
-    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
-}
-
 document.addEventListener("DOMContentLoaded", () => {
     /* Fix sticky sidebar margins when scrolling down */
     let observer = new IntersectionObserver(([e]) => {
@@ -10,39 +6,36 @@ document.addEventListener("DOMContentLoaded", () => {
         e.target.querySelector(".sidebar-button").classList.toggle('-top-1', e.intersectionRatio === 1);
         e.target.querySelector(".sidebar-panel").classList.toggle('-top-1', e.intersectionRatio === 1);
     }, {threshold: [1]});
-    observer.observe(document.getElementById("content-page-sidebar"));
+    observer.observe(document.getElementById("content-page-sidebar-mobile"));
 
-    /* Display sidebar panel on click */
-    document.querySelector("#content-page-sidebar .sidebar-button").addEventListener("click", (event) => {
+    /* Display mobile sidebar panel on click */
+    document.querySelector("#content-page-sidebar-mobile .sidebar-button").addEventListener("click", (event) => {
         event.target.closest("div").classList.add("hidden");
-        document.querySelector("#content-page-sidebar .sidebar-panel").classList.remove("hidden");
+        document.querySelector("#content-page-sidebar-mobile .sidebar-panel").classList.remove("hidden");
     });
 
-    /* Close sidebar panel on click */
+    /* Close mobile sidebar panel on click */
     document.addEventListener("click", (event) => {
-        if (!document.querySelector("#content-page-sidebar").contains(event.target)) {
-            document.querySelector("#content-page-sidebar .sidebar-panel").classList.add("hidden");
-            document.querySelector("#content-page-sidebar .sidebar-button").classList.remove("hidden");
+        if (!document.querySelector("#content-page-sidebar-mobile").contains(event.target)) {
+            document.querySelector("#content-page-sidebar-mobile .sidebar-panel").classList.add("hidden");
+            document.querySelector("#content-page-sidebar-mobile .sidebar-button").classList.remove("hidden");
         }
     });
 
-    /* Highlight entries in sidebar as users scroll to them */
-    let headers = document.querySelectorAll(".content-page-body h2, .content-page-body h3");
-    document.addEventListener("scroll", () => {
-        /* Check if we've reached the top */
-        if (window.scrollY === 0) {
-            console.log("sir, we've landed on the moon. You're not going to like what you hear, but we're not the first ones to have done so... and these don't look like human footprints.")
+    /* Adjust position of desktop sidebar when viewport changes */
+    function adjust_desktop_sidebar_position() {
+        let desktop_sidebar = document.getElementById("content-page-sidebar-desktop"),
+            content_div = document.getElementById("content-div");
+        if (window.innerWidth - content_div.getBoundingClientRect().right < desktop_sidebar.offsetWidth) {
+            desktop_sidebar.style.right = window.innerWidth - content_div.getBoundingClientRect().right+ 5 + "px";
+            desktop_sidebar.getElementsByTagName("ul")[0].classList.add("bg-slate-700");
+        } else {
+            desktop_sidebar.style.right = window.innerWidth - content_div.getBoundingClientRect().right - desktop_sidebar.offsetWidth + "px";
+            desktop_sidebar.getElementsByTagName("ul")[0].classList.remove("bg-slate-700");
         }
-        /* Check if we've reached the bottom */
-        if ((window.innerHeight + Math.round(window.scrollY)) >= document.documentElement.scrollHeight) {
-            console.log("sir, we've reached the bottom of the marina trench.");
-        }
+    }
+    adjust_desktop_sidebar_position();
+    visualViewport.addEventListener("resize", () => {
+        adjust_desktop_sidebar_position();
     });
-    let header_observer = new IntersectionObserver(([e]) => {
-        if (e.isIntersecting) {
-            console.log(e.target);
-            console.log("ah yeah we active.");
-        }
-    }, {rootMargin: "-95% 0px 0px 0px"});
-    headers.forEach(header => {header_observer.observe(header)});
 });
