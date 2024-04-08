bd4ce38f367995ab614a919db354788670b5b803
Ciaran Sanders <ciaransanders@gmail.com>
2024-04-08 11:58:44 -0600

    removed apsect-ratio tailwindcss package, using base aspect-ratio utility classes from tailwindcss instead

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -65,7 +65,6 @@ module.exports = {
     plugins: [
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
-        require('@tailwindcss/aspect-ratio'),
     ],
     safelist: [
         {
