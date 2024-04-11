d95b26f98796acebd66eee43f155d20bdb6190f1
Ciaran Sanders <ciaransanders@gmail.com>
2024-04-11 10:27:00 -0600

    removed staff_member_required decorator from bounty views

--- bounty/urls.py
+++ bounty/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import path
 
 from bounty import views
@@ -8,22 +7,22 @@ app_name = "bounty"
 urlpatterns = [
     path(
         "",
-        staff_member_required(views.CurrentBountyView.as_view()),
+        views.CurrentBountyView.as_view(),
         name="current-bounty",
     ),
     path(
         "index/",
-        staff_member_required(views.BountyIndexView.as_view()),
+        views.BountyIndexView.as_view(),
         name="bounty-index",
     ),
     path(
         "<int:id>/",
-        staff_member_required(views.BountyView.as_view()),
+        views.BountyView.as_view(),
         name="bounty-view",
     ),
     path(
         "rules/",
-        staff_member_required(views.BountyRulesView.as_view()),
+        views.BountyRulesView.as_view(),
         name="bounty-rules",
     ),
 ]
