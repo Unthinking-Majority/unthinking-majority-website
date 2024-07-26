2f7d8f6bb281b6c0389558e96ffe6d7784f0298b
Ciaran Sanders <ciaransanders@gmail.com>
2024-07-25 20:41:16 -0600

    changed rest permissions to remove Anon readonly

--- um/settings.py
+++ um/settings.py
@@ -214,7 +214,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
+        "rest_framework.permissions.DjangoModelPermissions",
     ],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
